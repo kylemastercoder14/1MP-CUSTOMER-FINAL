69f11c6a17c7f4531a42a4c8344b1a7946e95e81
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-10-10 11:15:56 +0800

    fixed build issues

--- src/app/(routes)/report-abuse/submit-a-report/page.tsx
+++ src/app/(routes)/report-abuse/submit-a-report/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import React, { Suspense } from "react";
 import Client from "./client";
 import { useUser } from "@/hooks/use-user";
