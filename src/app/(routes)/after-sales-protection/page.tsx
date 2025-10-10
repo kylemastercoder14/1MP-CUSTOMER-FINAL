0d3583c36c5f81af1ee344486ee05f4c6c65401b
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-10-10 10:23:39 +0800

    fixed build issues

--- src/app/(routes)/after-sales-protection/page.tsx
+++ src/app/(routes)/after-sales-protection/page.tsx
@@ -1,16 +1,13 @@
-"use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
-import React, { Suspense } from 'react'
-import Client from './client';
+import React, { Suspense } from "react";
+import Client from "./client";
 
-export const dynamic = "force-dynamic"; // ⬅ prevents static prerendering
+const Page = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Client />
+  </Suspense>
+);
 
-const Page = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Client />
-    </Suspense>
-  );
-}
-
-export default Page
+export default Page;
