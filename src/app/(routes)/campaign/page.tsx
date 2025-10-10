cfddfa94ae669bfc4bb7d827ca6795eecfdce7e1
jeffreelingal011 <jeffreelingal00@gmail.com>
2025-10-10 11:04:25 +0800

    fixed build issues

--- src/app/(routes)/campaign/page.tsx
+++ src/app/(routes)/campaign/page.tsx
@@ -1,16 +1,13 @@
-"use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import React, { Suspense } from "react";
 import Client from "./client";
 
-export const dynamic = "force-dynamic"; // ⬅ prevents static prerendering
-
-const Page = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Client />
-    </Suspense>
-  );
-};
+const Page = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Client />
+  </Suspense>
+);
 
 export default Page;
--- src/app/(routes)/vendor/page.tsx
+++ src/app/(routes)/vendor/page.tsx
@@ -1,12 +1,13 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import React, { Suspense } from "react";
 import Client from "./client";
 
-const Page = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Client />
-    </Suspense>
-  );
-};
+const Page = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Client />
+  </Suspense>
+);
 
 export default Page;
